0369bc6face322e11d17ddbe6ca6ce6af6965e98
TomaszPosluszny <tomkposluszny@gmail.com>
2023-01-03 18:40:24 +0100

    add note

--- js/notes.js
+++ js/notes.js
@@ -4,17 +4,39 @@ const cancelNote = document.querySelector('.buttons__cancel');
 const deleteBtn = document.getElementsByClassName('pushbutton__delete');
 
 const notePanel = document.querySelector('.addnote');
-
+const error = document.querySelector('.addnote__error');
+const textarea = document.querySelector('#text');
+const category = document.querySelector('#category');
 
 const showPanel = () => {
 	notePanel.style.display = 'flex';
 };
 
 const closePanel = () => {
-    notePanel.style.display = 'none';
-   
-}
+	notePanel.style.display = 'none';
+	error.style.visibility = 'hidden';
+	textarea.value = '';
+	category.selectedIndex = 0;
+};
+const addError = () => {
+	error.style.display = 'flex';
+};
+
+const deleteError = () => {
+	error.style.display = 'none';
+};
+const addNote = () => {
+	if (
+		textarea.value !== '' &&
+		category.options[category.selectedIndex].value !== '0'
+	) {
+		createNote();
+		deleteError();
+	} else {
+		addError();
+	}
+};
 
 addBtn.addEventListener('click', showPanel);
-cancelNote.addEventListener('click', closePanel)
-
+cancelNote.addEventListener('click', closePanel);
+saveNote.addEventListener('click', addNote);
