c4fc4f55ceb1151c836818a133ec3b150b307e00
TomaszPosluszny <tomkposluszny@gmail.com>
2023-01-04 18:16:26 +0100

    add creatNote

--- js/notes.js
+++ js/notes.js
@@ -8,6 +8,11 @@ const error = document.querySelector('.addnote__error');
 const textarea = document.querySelector('#text');
 const category = document.querySelector('#category');
 
+const noteArea = document.querySelector('.note');
+
+let selectedValue;
+let cardID = 0;
+
 const showPanel = () => {
 	notePanel.style.display = 'flex';
 };
@@ -36,7 +41,36 @@ const addNote = () => {
 		addError();
 	}
 };
+const createNote = () => {
+    const newNote = document.createElement('div');
+    newNote.classList.add('note__area');
+    newNote.setAttribute('id', cardID);
 
+    newNote.innerHTML = `
+        <div class="note__header">
+        <h3 class="note__title">${selectedValue}</h3>
+        <button class="note__delete" onclick="deleteNote(${cardID})">
+            <i class="fas fa-times icon"></i>
+        </button>
+        </div>
+        <div class="note__body">
+            ${textarea.value}
+        </div>  
+    `
+
+    noteArea.appendChild(newNote);
+    cardID++;
+    textarea.value = '';
+    category.selectedIndex = 0;
+    notePanel.style.display = 'none';
+   
+}
+
+const selectValue = () => {
+    selectedValue = category.options[category.selectedIndex].text;
+}
+
+selectValue()
 addBtn.addEventListener('click', showPanel);
 cancelNote.addEventListener('click', closePanel);
 saveNote.addEventListener('click', addNote);
