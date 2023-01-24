5e3107872e3657435b14d5e7b4370082564db7de
TomaszPosluszny <tomkposluszny@gmail.com>
2023-01-24 19:53:42 +0100

    add function showError, clearError, checkForm

--- js/registration.js
+++ js/registration.js
@@ -6,11 +6,39 @@ const sendBtn = document.querySelector('.area__buttons--send');
 const clearBtn = document.querySelector('.area__buttons--clear');
 const info = document.querySelector('.area__popup');
 
-clearBtn.addEventListener('click', e => {
-    e.preventDefault();
+const showError = (input, msg) => {
+	const formBox = input.parentElement;
+	const errorMsg = formBox.querySelector('.area__error');
 
-    [username, password, passwordTwo, email].forEach(el => {
-        el.value = ''
-    })
-   
-})
+	formBox.classList.add('error');
+	errorMsg.textContent = msg;
+};
+
+const clearError = (input) => {
+	const formBox = input.parentElement;
+	formBox.classList.remove('error');
+};
+
+const checkForm = (input) => {
+	input.forEach((el) => {
+		if (el.value === '') {
+			showError(el, el.placeholder);
+		} else {
+			clearError(el);
+		}
+	});
+};
+
+sendBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	checkForm([username, password, passwordTwo, email]);
+});
+
+clearBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	[username, password, passwordTwo, email].forEach((el) => {
+		el.value = '';
+	});
+});
